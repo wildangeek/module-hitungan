a9d9242f06f11da266c3eff25abf1ecdf32a0919
wildangeek <wildanbeasiswa@gmail.com>
2020-06-19 15:09:08 +0700

    hitungan

---
+++ script2.js
@@ -0,0 +1,17 @@
+function tambah(ini, itu) {
+    return ini + itu;
+  }
+  
+  function kurang(ini, itu) {
+    return ini - itu;
+  }
+  
+  function kali(ini, itu) {
+    return ini * itu;
+  }
+  
+  function bagi(ini, itu) {
+    return ini / itu;
+  }
+  
+  export { tambah, kurang, kali, bagi };
