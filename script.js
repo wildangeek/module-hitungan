98af7c16e71cf9d3587139524e3a8fc1a4054794
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-06-19 15:16:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ import { tambah, kurang, kali, bagi } from "./script2.js";
 let ini = 10;
 
 console.log(`${ini} + ${ini} = ${tambah(10, 10)}`);
-console.log(`${ini} + ${ini} = ${kurang(10, 10)}`);
-console.log(`${ini} + ${ini} = ${kali(10, 10)}`);
-console.log(`${ini} + ${ini} = ${bagi(10, 10)}`);
+console.log(`${ini} - ${ini} = ${kurang(10, 10)}`);
+console.log(`${ini} * ${ini} = ${kali(10, 10)}`);
+console.log(`${ini} / ${ini} = ${bagi(10, 10)}`);
